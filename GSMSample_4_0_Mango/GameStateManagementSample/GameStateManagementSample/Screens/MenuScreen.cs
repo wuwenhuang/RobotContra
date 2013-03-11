cb18075d7d311f4b4537b6c532489e3543cfc793
Dexter <wuwenhuang88@qq.com>
2013-03-11 15:46:39 -0400

    add background

--- GSMSample_4_0_Mango/GameStateManagementSample/GameStateManagementSample/Screens/MenuScreen.cs
+++ GSMSample_4_0_Mango/GameStateManagementSample/GameStateManagementSample/Screens/MenuScreen.cs
@@ -176,7 +176,7 @@ namespace GameStateManagementSample
             float transitionOffset = (float)Math.Pow(TransitionPosition, 2);
 
             // start at Y = 175; each X value is generated per entry
-            Vector2 position = new Vector2(0f, 175f);
+            Vector2 position = new Vector2(0f, 220f);
 
             // update each menu entry's location in turn
             for (int i = 0; i < menuEntries.Count; i++)
@@ -248,10 +248,10 @@ namespace GameStateManagementSample
             float transitionOffset = (float)Math.Pow(TransitionPosition, 2);
 
             // Draw the menu title centered on the screen
-            Vector2 titlePosition = new Vector2(graphics.Viewport.Width / 2, 80);
+            Vector2 titlePosition = new Vector2(graphics.Viewport.Width / 2, 150);
             Vector2 titleOrigin = font.MeasureString(menuTitle) / 2;
-            Color titleColor = new Color(192, 192, 192) * TransitionAlpha;
-            float titleScale = 1.25f;
+            Color titleColor = new Color(255, 255, 255) * TransitionAlpha;
+            float titleScale = 2f;
 
             titlePosition.Y -= transitionOffset * 100;
 
